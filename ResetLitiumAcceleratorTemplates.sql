c840b290d40effe4971543e558c212c76f09ea1c
Jonas Kastebo <kastebo@gmail.com>
2020-08-17 11:24:03 +0200

    Create ResetLitiumAcceleratorTemplates.sql

---
+++ ResetLitiumAcceleratorTemplates.sql
@@ -0,0 +1,6 @@
+// TODO: Remove template fields
+// TODO: Removed templates
+// TODO: Reset acceleator filter fields
+// TODO: Reset accelerator website filterfields
+// TODO: Reset accelerator category filterfields
+// TODO: Reset accelerator fielddefinitionssettings from Commonnsettings
