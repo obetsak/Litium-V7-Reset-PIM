fca400387ab35365e0e05aeae12f301d4533e82f
Jonas Kastebo <kastebo@gmail.com>
2020-08-17 13:00:11 +0200

    Update ResetLitiumAcceleratorTemplates.sql

--- ResetLitiumAcceleratorTemplates.sql
+++ ResetLitiumAcceleratorTemplates.sql
@@ -3,7 +3,10 @@
 // TODO: Reset acceleator filter fields
 // TODO: Reset accelerator website filterfields
 // TODO: Reset accelerator category filterfields
+
 // TODO: Reset accelerator fielddefinitionssettings from Commonnsettings
+// Needed if templates and fields removed otherwise accelerator will not recreate them.
+// It is a good time to take a oversight of the translations for the field also if there are many misspelled texts for a specific language. (See accelerator resourcefiles)
 DELETE FROM [Common].[Setting]
   where [Key] like'acceleratorbuilder:litium.fieldframework.fielddefinition:productarea%'
   or  [Key] like'acceleratorbuilder:litium.fieldframework.fieldtemplate:productarea%'
