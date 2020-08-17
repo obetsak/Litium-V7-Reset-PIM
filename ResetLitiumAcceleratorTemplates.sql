6334b1299e1d67f0cc1ab8425e99daf2c357de77
Jonas Kastebo <kastebo@gmail.com>
2020-08-17 12:58:01 +0200

    Update ResetLitiumAcceleratorTemplates.sql
    commonsettings erase

--- ResetLitiumAcceleratorTemplates.sql
+++ ResetLitiumAcceleratorTemplates.sql
@@ -4,3 +4,6 @@
 // TODO: Reset accelerator website filterfields
 // TODO: Reset accelerator category filterfields
 // TODO: Reset accelerator fielddefinitionssettings from Commonnsettings
+DELETE FROM [Common].[Setting]
+  where [Key] like'acceleratorbuilder:litium.fieldframework.fielddefinition:productarea%'
+  or  [Key] like'acceleratorbuilder:litium.fieldframework.fieldtemplate:productarea%'
